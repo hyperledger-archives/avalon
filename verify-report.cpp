68ea9401ae1c9ce753712945285538254a826f33
Byron Marohn <byron.marohn@intel.com>
2018-09-30 12:22:15 -0700

    Add an UNTRUSTED_ONLY compile option to the common directory
    Use this like:
    	cmake -DUNTRUSTED_ONLY=ON ..
    
    Signed-off-by: Byron Marohn <byron.marohn@intel.com>

--- verify-report.cpp
+++ verify-report.cpp
@@ -64,7 +64,7 @@ void get_quote_from_report(const uint8_t* report, const int report_len, sgx_quot
     memcpy_s(buf, buf_len, report, report_len);
     buf[report_len] = '\0';
 
-    int json_string_max_len = 64;
+    const int json_string_max_len = 64;
     const char json_string[json_string_max_len] = "\"isvEnclaveQuoteBody\":\"";
     char* p_begin = strstr(buf, json_string);
     assert(p_begin != NULL);
@@ -183,20 +183,20 @@ verify_status_t verify_enclave_quote_status(const char* ias_report, int ias_repo
     memcpy_s(buf, buf_len, ias_report, ias_report_len);
     buf[ias_report_len] = '\0';
 
-    int json_string_max_len = 64;
+    const int json_string_max_len = 64;
     const char json_string[json_string_max_len] = "\"isvEnclaveQuoteStatus\":\"";
     char* p_begin = strstr(buf, json_string);
     assert(p_begin != NULL);
     p_begin += strnlen(json_string, json_string_max_len);
 
-    int status_OK_max_len = 8;
+    const int status_OK_max_len = 8;
     const char status_OK[status_OK_max_len] = "OK\"";
     if (0 == strncmp(p_begin, status_OK, strnlen(status_OK, status_OK_max_len)))
         return VERIFY_SUCCESS;
 
     if(group_out_of_date_is_ok)
     {
-        int status_outdated_max_len = 64;
+        const int status_outdated_max_len = 64;
         const char status_outdated[status_outdated_max_len] = "GROUP_OUT_OF_DATE\"";
         if (0 == strncmp(p_begin, status_outdated, strnlen(status_outdated, status_outdated_max_len)))
         {
