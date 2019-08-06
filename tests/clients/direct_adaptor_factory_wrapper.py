1834d96e3c8fff9f2d7112465aa4c014a8443cd6
Julian Kida <kida.julian@gmail.com>
2019-08-06 14:30:40 -0700

    1. Added echo result test client 2. Added hex string utilities
    Signed-off-by: Jucchan kida.julian@gmail.com
    Signed-off-by: Julian Kida <kida.julian@gmail.com>
    
    Changed argument and variable name in hex_utils
    
    Signed-off-by: Jucchan kida.julian@gmail.com
    Signed-off-by: Julian Kida <kida.julian@gmail.com>
    
    1. Added license information
    2. Added function comments
    3. Added newline at end of files
    4. Changed function names to snake case
    5. Removed unnecessary imports
    6. Separated lines that were too long
    
    Signed-off-by: Julian Kida <kida.julian@gmail.com>
    
    1. Changed variable names to snake case
    2. Separated long lines
    
    Signed-off-by: Julian Kida <kida.julian@gmail.com>
    
    1. Renamed workorder_json to json_rpc_request
    2. In json_rpc_request: renamed WorkOrderJson to JsonRpcRequest, added new WorkOrderJson class
    
    Signed-off-by: Julian Kida <kida.julian@gmail.com>
    
    1. Moved client files into new folder clients/
    2. Reduced line length to <=80
    3. Changed workloadId to be used as request type identifier
    4. Fixed issue in workload_idspatcher that ignored first echo indata
    
    Signed-off-by: Julian Kida <kida.julian@gmail.com>

---
+++ tests/clients/direct_adaptor_factory_wrapper.py
@@ -0,0 +1,189 @@
+# Copyright 2019 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+import time
+import sys
+import utils.utility as enclave_helper
+import utils.signature as signature
+from service_client.generic import GenericServiceClient
+from tcf_connector.direct_json_rpc_api_adaptor_factory \
+	import DirectJsonRpcApiAdaptorFactory
+import utils.hex_utils as hex_utils
+import json_rpc_request
+
+logger = logging.getLogger(__name__)
+# -----------------------------------------------------------------
+class DirectAdaptorFactoryWrapper:
+	# Contains the functions needed to submit and retrieve work orders in the Direct Model
+	def __init__(self, config_file):
+		self.adaptor_factory = DirectJsonRpcApiAdaptorFactory(config_file)
+		self.worker_registry_list = None
+		self.worker_registry = None
+		self.work_order = None
+		self.work_order_receipt = None
+
+	def init_worker_registry_list(self, config):
+		self.worker_registry_list = \
+			self.adaptor_factory.create_worker_registry_list_adaptor(config)
+
+	# Lookup worker registries in registry list
+	def registry_lookup(self, app_type_id=None):
+		if self.worker_registry_list == None:
+			logger.error("ERROR: Worker registry list adaptor not initialized")
+			sys.exit(1)
+		if app_type_id:
+			lookup_result = self.worker_registry_list.registry_lookup(
+				app_type_id=app_type_id)
+		else:
+			lookup_result = self.worker_registry_list.registry_lookup()
+		logger.info("Registry Lookup result: [%d, %s, %s]", 
+			lookup_result[0], lookup_result[1], 
+			hex_utils.pretty_ids(lookup_result[2]))
+		return lookup_result
+
+	# Retrieve worker registry URI
+	def registry_retrieve(self, org_id):
+		if self.worker_registry_list == None:
+			logger.error("ERROR: Worker registry list adaptor not initialized")
+			sys.exit(1)
+		retrieve_result = self.worker_registry_list.registry_retrieve(org_id)
+		logger.info("Registry retrieved: [%s, %s, %s, %d]", 
+			retrieve_result[0], 
+			hex_utils.hex_to_utf(retrieve_result[1]), 
+			hex_utils.pretty_ids(retrieve_result[2]), 
+			retrieve_result[3])
+		return retrieve_result
+
+	def init_worker_registry(self, config):
+		self.worker_registry = \
+			self.adaptor_factory.create_worker_registry_adaptor(config)
+
+	# Lookup worker in worker registry
+	def worker_lookup(self, worker_lookup_json):
+		if self.worker_registry == None:
+			logger.error("ERROR: Worker registry adaptor not initialized")
+			sys.exit(1)
+
+		logger.info("*********Request Json********* \n%s\n", 
+			worker_lookup_json.get_json_str_indent())
+		response = self.worker_registry.worker_lookup(
+			worker_type=worker_lookup_json.get_worker_type(), 
+			organization_id=worker_lookup_json.get_organization_id(), 
+			application_type_id=worker_lookup_json.get_application_type_id(), 
+			id=worker_lookup_json.get_id())
+		logger.info("**********Received Response*********\n%s\n", 
+			json.dumps(response, indent=4))
+
+		return response
+
+	# Retrieve worker from worker registry
+	def worker_retrieve(self, worker_retrieve_json):
+		if self.worker_registry == None:
+			logger.error("ERROR: Worker registry adaptor not initialized")
+			sys.exit(1)
+
+		logger.info("*********Request Json********* \n%s\n", 
+			worker_retrieve_json.get_json_str_indent())
+		response = self.worker_registry.worker_retrieve(
+			worker_retrieve_json.get_worker_id(), 
+			id=worker_retrieve_json.get_id())
+		logger.info("**********Received Response*********\n%s\n", 
+			json.dumps(response, indent=4))
+
+		if "result" in response:
+			return response
+		else:
+			logger.error("ERROR: Failed to retrieve worker")
+			sys.exit(1)
+
+	def init_work_order(self, config):
+		self.work_order = \
+			self.adaptor_factory.create_work_order_adaptor(config)
+
+	# Encrypt work order and submit
+	def work_order_submit(self, wo_submit_json, encrypted_session_key, 
+		worker_obj, private_key, session_iv):
+		if self.work_order == None:
+			logger.error("ERROR: Work order adaptor not initialized")
+			sys.exit(1)
+
+		input_json_str = wo_submit_json.get_json_str()
+		sig_obj = signature.ClientSignature()
+		input_json_str = sig_obj.generate_client_signature(
+			input_json_str, worker_obj,
+			private_key, session_iv, encrypted_session_key)
+		wo_submit_json.load_from_str(input_json_str)
+
+		logger.info("*********Request Json********* \n%s\n", 
+			json.dumps(json.loads(input_json_str), indent=4))
+		response = self.work_order.work_order_submit(
+			wo_submit_json.get_params(), wo_submit_json.get_in_data(), 
+			wo_submit_json.get_out_data(), id=wo_submit_json.get_id())
+		logger.info("**********Received Response*********\n%s\n", 
+			json.dumps(response, indent=4))
+
+		# Return signed workorder json
+		return input_json_str
+
+	# Retrieve work order result and decrypt response
+	def work_order_get_result(self, wo_get_result_json, encrypted_session_key):
+		if self.work_order == None:
+			logger.error("ERROR: Work order adaptor not initialized")
+			sys.exit(1)
+
+		logger.info("*********Request Json********* \n%s\n", 
+			wo_get_result_json.get_json_str_indent())
+		response = self.work_order.work_order_get_result(
+			wo_get_result_json.get_work_order_id(), 
+			wo_get_result_json.get_id())
+		logger.info("**********Received Response*********\n%s\n", 
+			json.dumps(response))
+
+		# Poll for the "WorkOrderGetResult" and break when you get the result
+		while("result" not in response):
+			if response["error"]["code"] == 9:
+				break
+			response = self.work_order.work_order_get_result(
+				wo_get_result_json.get_work_order_id(), 
+				wo_get_result_json.get_id())
+			if "result" not in response:
+				logger.info(" Received Response : %s, \n \n ", response)
+				time.sleep(3)
+			else:
+				logger.info(" Received Response : %s, \n \n ", 
+					json.dumps(response, indent=4))
+
+		return enclave_helper.decrypted_response(
+			json.dumps(response), encrypted_session_key)
+
+	def init_work_order_receipt(self, config):
+		self.work_order_receipt = \
+			self.adaptor_factory.create_work_order_receipt_adaptor(config)
+
+	# Retrieve work order receipt
+	def work_order_receipt_retrieve(self, wo_receipt_retrieve_json):
+		if self.work_order_receipt == None:
+			logger.error("ERROR: Work order adaptor not initialized")
+			sys.exit(1)
+	
+		logger.info("*********Request Json********* \n%s\n", 
+			wo_receipt_retrieve_json.get_json_str_indent())
+		response = self.work_order_receipt.work_order_receipt_retrieve(
+			wo_receipt_retrieve_json.get_work_order_id(), 
+			wo_receipt_retrieve_json.get_id())
+		logger.info("**********Received Response*********\n%s\n", 
+			json.dumps(response, indent=4))
+		return response
---
+++ tests/clients/echo_client.py
@@ -0,0 +1,189 @@
+# Copyright 2019 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+import random
+import json
+import argparse
+import logging
+
+import config.config as pconfig
+import utility.logger as plogger
+from service_client.generic import GenericServiceClient
+import utils.utility as enclave_helper
+import worker.worker_details as worker
+import json_rpc_request as jrpc_request
+from direct_adaptor_factory_wrapper import DirectAdaptorFactoryWrapper
+
+# Remove duplicate loggers
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+logger = logging.getLogger(__name__)
+TCFHOME = os.environ.get("TCF_HOME", "../../")
+
+def ParseCommandLine(args) :
+
+	global worker_obj
+	global worker_id
+	global message
+	global config
+	global off_chain
+
+	parser = argparse.ArgumentParser()
+	use_service = parser.add_mutually_exclusive_group()
+	parser.add_argument("-c", "--config", 
+		help="the config file containing the Ethereum contract information", 
+		type=str)
+	use_service.add_argument("-r", "--registry-list", 
+		help="the Ethereum address of the registry list", 
+		type=str)
+	use_service.add_argument("-s", "--service-uri", 
+		help="skip URI lookup and send to specified URI", 
+		type=str)
+	use_service.add_argument("-o", "--off-chain", 
+		help="skip URI lookup and use the registry in the config file", 
+		action="store_true")
+	parser.add_argument("-w", "--worker-id", 
+		help="skip worker lookup and retrieve specified worker", 
+		type=str)
+	parser.add_argument("-m", "--message", 
+		help='text message to be included in the JSON request payload', 
+		type=str)
+
+	options = parser.parse_args(args)
+
+	if options.config:
+		conffiles = [options.config]
+	else:
+		conffiles = [ TCFHOME + "/common/tcf_connector/tcf_connector.toml" ]
+	confpaths = [ "." ]
+	try :
+		config = pconfig.parse_configuration_files(conffiles, confpaths)
+		config_json_str = json.dumps(config)
+	except pconfig.ConfigurationException as e :
+		logger.error(str(e))
+		sys.exit(-1)
+
+	global direct_wrapper
+	direct_wrapper = DirectAdaptorFactoryWrapper(conffiles[0])
+
+	# Whether or not to connect to the registry list on the blockchain
+	off_chain = False
+
+	if options.registry_list:
+		config["ethereum"]["direct_registry_contract_address"] = \
+			options.registry_list
+
+	if options.service_uri:
+		config["tcf"]["json_rpc_uri"] = options.service_uri
+		off_chain = True
+
+	if options.off_chain:
+		off_chain = True
+
+	worker_id = options.worker_id
+	message = options.message
+	if options.message == None or options.message == "":
+		message = "Test Message"
+
+	# Initializing Worker Object
+	worker_obj = worker.WorkerDetails()
+
+def Main(args=None):
+	ParseCommandLine(args)
+
+	config["Logging"] = {
+		"LogFile" : "__screen__",
+		"LogLevel" : "INFO"
+	}
+	
+	plogger.setup_loggers(config.get("Logging", {}))
+	sys.stdout = plogger.stream_to_logger(
+		logging.getLogger("STDOUT"), logging.DEBUG)
+	sys.stderr = plogger.stream_to_logger(
+		logging.getLogger("STDERR"), logging.WARN)
+
+	logger.info("***************** INTEL TRUSTED COMPUTE FRAMEWORK (TCF)" + \
+		" *****************") 
+
+	# Connect to registry list and retrieve registry
+	if not off_chain:
+		direct_wrapper.init_worker_registry_list(config)
+		registry_lookup_result = direct_wrapper.registry_lookup()
+		if (registry_lookup_result[0] == 0):
+			logger.warn("No registries found")
+			sys.exit(1)
+		registry_retrieve_result = direct_wrapper.registry_retrieve(
+			registry_lookup_result[2][0])
+		config["tcf"]["json_rpc_uri"] = registry_retrieve_result[0]
+
+	# Prepare worker
+	direct_wrapper.init_worker_registry(config)
+
+	global worker_id
+	if not worker_id:
+		worker_lookup_json = jrpc_request.WorkerLookupJson(1, worker_type=1)
+		worker_lookup_result = direct_wrapper.worker_lookup(worker_lookup_json)
+		if "result" in worker_lookup_result and \
+			"ids" in worker_lookup_result["result"].keys():
+			if worker_lookup_result["result"]["totalCount"] != 0:
+				worker_id = worker_lookup_result["result"]["ids"][0]
+			else:
+				logger.error("ERROR: No workers found")
+				sys.exit(1)
+		else:
+			logger.error("ERROR: Failed to lookup worker")
+			sys.exit(1)
+
+	worker_retrieve_json = jrpc_request.WorkerRetrieveJson(2, worker_id)
+	worker_obj.load_worker(
+		direct_wrapper.worker_retrieve(worker_retrieve_json))
+
+	logger.info("**********Worker details Updated with Worker ID" + \
+		"*********\n%s\n", worker_id)
+
+	# Create, sign, and submit workorder 
+	# Convert workloadId to hex
+	workload_id = "echo-result"
+	workload_id = workload_id.encode("UTF-8").hex()
+	wo_submit_json = jrpc_request.WorkOrderSubmitJson(3, 6000, "pformat",
+		worker_id, workload_id, "0x2345", 
+		worker_encryption_key=worker_obj.worker_encryption_key, 
+		data_encryption_algorithm="AES-GCM-256")
+	wo_id = wo_submit_json.get_work_order_id()
+	wo_submit_json.add_in_data("bcde1111", message)
+
+	private_key = enclave_helper.generate_signing_keys()
+	session_iv = enclave_helper.generate_sessioniv()
+	encrypted_session_key = enclave_helper.generate_encrypted_session_key(
+		session_iv, worker_obj.worker_encryption_key)
+
+	direct_wrapper.init_work_order(config)
+	direct_wrapper.work_order_submit(wo_submit_json, encrypted_session_key, 
+		worker_obj, private_key, session_iv)
+
+	# Retrieve result
+	wo_get_result_json = jrpc_request.WorkOrderGetResultJson(4, wo_id)
+	direct_wrapper.work_order_get_result(
+		wo_get_result_json, encrypted_session_key)
+
+	# Retrieve receipt
+	wo_receipt_retrieve_json = \
+		jrpc_request.WorkOrderReceiptRetrieveJson(5, wo_id)
+	direct_wrapper.init_work_order_receipt(config)
+	direct_wrapper.work_order_receipt_retrieve(wo_receipt_retrieve_json)
+
+#------------------------------------------------------------------------------
+Main()
