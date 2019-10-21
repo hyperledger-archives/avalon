b98760cdf573785b50dd40c2183e5d91c397039f
manju956 <manjunath.a.c@intel.com>
2019-10-21 00:07:59 -0700

    Remove virtual environment references in docker
    Signed-off-by: manju956 <manjunath.a.c@intel.com>

--- scripts/lmdb.sh
+++ scripts/lmdb.sh
@@ -15,8 +15,6 @@
 
 lmdb_server="${TCF_HOME}/examples/common/python/shared_kv/remote_lmdb/lmdb_listener.py"
 
-source ${TCF_HOME}/tools/build/_dev/bin/activate
-
 echo "start the lmdb server ..."
 
 python3 ${lmdb_server}
