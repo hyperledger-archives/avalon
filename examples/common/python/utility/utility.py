a38fd38e148482f1f6322244056655115c8f9a3f
Rajeev Ranjan <rajeev2.ranjan@intel.com>
2019-11-08 13:25:11 +0530

    Refactor functions within utilities
    Signed-off-by: Rajeev Ranjan <rajeev2.ranjan@intel.com>

--- examples/common/python/utility/utility.py
+++ examples/common/python/utility/utility.py
@@ -17,7 +17,6 @@ utility.py -- general helper function
 """
 import base64
 import os
-import json
 import utility.file_utils as putils
 import utility.hex_utils as hex_utils
 import crypto.crypto as crypto
@@ -30,59 +29,6 @@ TCFHOME = os.environ.get("TCF_HOME", "../../")
 # No of bytes of encrypted session key to encrypt data
 NO_OF_BYTES = 16
 
-def read_toml_file(input_file, config_name = None, confpaths = [".", TCFHOME + "/" + "config"]):
-    """
-    Function to read toml file and returns the toml content as a list
-    Parameters:
-        - input_file is any toml file which need to be read
-        - config_name is particular configuration to pull
-        - data_dir is the directory structure in which the toml file exists
-    """
-    conffiles = [input_file]
-    config = pconfig.parse_configuration_files(conffiles, confpaths)
-    if config_name is None:
-        return config
-    else :
-        result = config.get(config_name)
-        if result is None:
-            logger.error("%s is missing in toml file %s", config_name, input_file )
-            return None
-        else :
-            return result
-
-#---------------------------------------------------------------------------------------------
-def read_json_file(input_file, data_dir = ['./', '../', '/']) :
-    """
-    Function to read json file and returns the json content as a string
-    Parameters:
-        - input_file is any json file which need to be read
-        - data_dir is the directory structure in which the json file exists
-    """
-
-    file_name = putils.find_file_in_path(input_file, data_dir)
-    with open(file_name, "r") as input_json_file :
-        input_json = input_json_file.read()
-    return input_json
-
-#---------------------------------------------------------------------------------------------
-def write_json_file(file_name,input_data, data_dir ='./') :
-    """
-    Function to store data as json file
-    Parameters:
-        - file_name is the name in which the file should be stored
-        - input_data is any data which needs to be stored in a file
-        - data_dir is the directory path to store the file
-    """
-
-    logger.debug('Data file is stored at %s with name %s.json',data_dir, file_name)
-    result_info = dict()
-    result_info['Result'] = input_data.result
-    filename = os.path.realpath(os.path.join(data_dir, file_name + ".json"))
-    logger.debug('save result data to %s', filename)
-    with open(filename, "w") as file :
-        json.dump(result_info, file)
-
-#---------------------------------------------------------------------------------------------
 def create_error_response(code, jrpc_id, message):
     """
     Function to create error response
