c8850109f28ae5f0b8d0d008f00bc7958605ea41
manju956 <manjunath.a.c@intel.com>
2019-11-14 06:48:26 -0800

    Implementation of untrusted Inside-out API
    Signed-off-by: manju956 <manjunath.a.c@intel.com>

---
+++ tc/sgx/trusted_worker_manager/enclave_wrapper/io_handler_if.h
@@ -0,0 +1,30 @@
+/* Copyright 2019 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdlib.h>
+
+class IoHandlerInterface {
+public:
+    virtual uint32_t Process(uint32_t handlerId,
+                             const uint8_t* command,
+                             size_t commandSize,
+                             uint8_t* result,
+                             size_t resultSize,
+                             const uint8_t* inBuf,
+                             size_t inBufSize,
+                             uint8_t* outBuf,
+                             size_t outBufSize) = 0;
+};
+
