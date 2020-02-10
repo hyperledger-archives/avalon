1b7422804c768eabec0635bba564bd4e6803b3a4
Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>
2020-02-10 00:13:44 -0800

    Optimize Shared KV docker build.
    - Added support for multi stage docker build. Shared KV dockerfile
      will include only required files in container image.
    - Remove dependency of swig utils header file.
    
    Signed-off-by: Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>

--- shared_kv_storage/setup.py
+++ shared_kv_storage/setup.py
@@ -56,11 +56,7 @@ if debug_flag:
 
 include_dirs = [
     module_src_path,
-    os.path.join(tcf_root_dir, 'common/cpp'),
-    os.path.join(
-      tcf_root_dir,
-      'tc/sgx/trusted_worker_manager/enclave_untrusted/enclave_bridge_wrapper'
-    )
+    os.path.join(tcf_root_dir, 'common/cpp')
 ]
 
 library_dirs = [
