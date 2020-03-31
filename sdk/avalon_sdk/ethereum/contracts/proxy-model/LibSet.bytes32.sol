e429bdd0e2cad7513530be763d07700fa54da2c9
Hadrien Croubois <hc@iex.ec>
2020-03-31 18:44:28 +0200

    fixing typos + adding mapping details
    Signed-off-by: Hadrien Croubois <hc@iex.ec>

--- sdk/avalon_sdk/ethereum/contracts/proxy-model/LibSet.bytes32.sol
+++ sdk/avalon_sdk/ethereum/contracts/proxy-model/LibSet.bytes32.sol
@@ -1,4 +1,4 @@
-/* Copyright 2019 iExec Blockchain Tech
+/* Copyright 2020 iExec Blockchain Tech
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
