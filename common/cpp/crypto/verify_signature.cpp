b8cff905a57b036ef4051f064865d7e71210f507
danintel <daniel.anderson@intel.com>
2020-05-18 15:55:39 -0700

    Add MbedTLS signature verification as an alternative to OpenSSL
    - Add code for RSA signature verification with a X.509 certificate
    - Move base64_decoded_length() to base64.cpp to prevent duplicate code
    
    Signed-off-by: danintel <daniel.anderson@intel.com>

--- common/cpp/crypto/verify_signature.cpp
+++ common/cpp/crypto/verify_signature.cpp
@@ -21,45 +21,25 @@
 #include <openssl/evp.h>
 
 #include "crypto_shared.h"
+#include "base64.h"           // base64_decoded_length()
 #include "verify_signature.h"
 
-
-/*
- * EVP_DecodeBlock pads its output with \0 if the output length is not
- * a multiple of 3. Check if the base64 string is padded at the end with '='
- * and adjust the output length.
- * For details, see the OpenSSL EVP_EncodeInit man page.
- */
-#define ADJUST_DECODE_LENGTH(in, in_len) \
-    ((((in)[(in_len) - 1] == '=') ? -1 : 0) + \
-     (((in)[(in_len) - 2] == '=') ? -1 : 0))
-
-
-/* Calculate the length of a base 64 encoded string after decoding. */
-static unsigned int
-base64_decoded_length(const char *encoded, unsigned int encoded_len)
-{
-    if (encoded == nullptr || encoded_len == 0) {
-        return 0;
-    }
-
-    unsigned int len = (encoded_len / 4) * 3;
-
-    if (encoded_len > 3)
-        len += ADJUST_DECODE_LENGTH(encoded, encoded_len);
-    return (len);
-}
+#ifndef CRYPTOLIB_OPENSSL
+#error "CRYPTOLIB_OPENSSL must be defined to compile source with OpenSSL."
+#endif
 
 
 /**
- * Verify a signature given a message, signature, and cert.
+ * Verify a RSA signature given a message, signature, and cert.
  * Use SHA-256 to hash the message.
+ * Use OAEP padding (not PKCS#1 v1.5).
+ * Use PKCS#1 v1.5 encryption scheme (not PSS).
  *
  * @param cert_pem X.509 Certificate to verify
- *                 with BEGIN end BLOCKS and new lines
+ *                 with BEGIN and END BLOCKS and new lines
  * @param msg Message to verify
  * @param msg_len Length of msg
- * @param signature Signature to verify message, base64 encoded
+ * @param signature RSA signature to verify message, base64 encoded
  * @param signature_len Length of signature
  * @returns true on success and false on failure.
  */
@@ -67,7 +47,7 @@ bool verify_signature(const char* cert_pem,
     const char* msg, unsigned int msg_len,
     const char* signature, unsigned int signature_len)
 {
-    // DSA or RSA keys larger than 16384 are not in practical use in the
+    // RSA keys larger than 16384 are not in practical use in the
     // near future. Increase buffer size if necessary.
     static const unsigned int max_decoded_len = 2048; // <=16384 bit signatures
     // Decoded result may have 1-2 extra '\0' bytes + ending '\0' byte.
@@ -81,14 +61,17 @@ bool verify_signature(const char* cert_pem,
     if (decoded_len > max_decoded_len)
         return false;
 
+    // Parse X.509 cert
     BIO* crt_bio = BIO_new_mem_buf((void*)cert_pem, -1);
 
     X509* crt = PEM_read_bio_X509(crt_bio, nullptr, 0, nullptr);
     assert(crt != nullptr);
 
+    // Extract RSA public key (with N and E) from X.509 cert
     EVP_PKEY* key = X509_get_pubkey(crt);
     assert(key != nullptr);
 
+    // Perform SHA-256 hash on message
     EVP_MD_CTX* ctx = EVP_MD_CTX_create();
     ret = EVP_VerifyInit_ex(ctx, EVP_sha256(), nullptr);
     assert(ret == 1);
@@ -101,6 +84,7 @@ bool verify_signature(const char* cert_pem,
         (unsigned char*)signature, signature_len);
     assert(ret != -1);
 
+    // Verify the signature against the hash digest and RSA public key (N & E)
     ret = EVP_VerifyFinal(ctx, (unsigned char*)signature_decoded, decoded_len,
         key);
 
