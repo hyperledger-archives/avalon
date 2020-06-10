388c80128c3b26e10f99c611b4e54d9d219948c8
Rajeev Ranjan <rajeev2.ranjan@intel.com>
2020-06-10 09:45:33 +0530

    Low level db changes to handle csv
     - Add CPP API to handle prepend, append, pop (remove from beginning)
       for value stored as csv
    
    Signed-off-by: Rajeev Ranjan <rajeev2.ranjan@intel.com>

--- shared_kv_storage/db_store/db_store.h
+++ shared_kv_storage/db_store/db_store.h
@@ -12,86 +12,91 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#pragma once
 
 #include <string>
 #include <map>
 
-/**
- * Initialize the database store - must be called before performing gets/puts
- *
- * @param db_path       path to the persistent database store database
- * @param map_size      the maximum size of the database
- */
-void db_store_init(const std::string& db_path, const size_t map_size);
+class DbStore {
+    public:
 
-/**
- * Close the database store - must be called when exiting
- *
- */
-void db_store_close();
+        DbStore() {}
 
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-/**
- * Gets the size of a key in the database store
- *
- * @param table_b64     base64 encode table name
- * @param key_b64       base64 encoded key string
- *
- * @return
- *  Success: length of value corresponding to key
- *  Failure: -1
- */
-int db_store_get_value_size(
-    const std::string& table_b64,
-    const std::string& key_b64);
+        /**
+         * Initialize the database store - must be called before performing gets/puts
+         *
+         * @param db_path       path/directory in which the database files reside
+         * @param map_size      the maximum size of the database
+         */
+        void db_store_init(const std::string& db_path, const size_t map_size);
 
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-/**
- * Gets the value corresponding to a key from the database store
- *
- * @param table_b64     base64 encode table name
- * @param key_b64       base64 encoded key string
- *
- * @return
- *  Success: base64 encoded value corresponding to key
- *  Failure: throws exception
- */
-std::string db_store_get(
-    const std::string& table_b64,
-    const std::string& key_b64);
+        /**
+         * Close the database store - must be called when exiting
+         *
+         */
+        void db_store_close();
 
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-/**
- * Puts a key->value pair into the database store
- *
- * @param table_b64     base64 encode table name
- * @param key_b64       base64 encoded key string
- * @param value_b64     base64 encoded value string
- *
- * @return
- *  Success: void/no return
- *  Failure: throws exception
- */
-void db_store_put(
-    const std::string& table_b64,
-    const std::string& key_b64,
-    const std::string& value_b64);
+        // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        /**
+         * Gets the size of a key in the database store
+         *
+         * @param table_b64     base64 encoded table name
+         * @param key_b64       base64 encoded key string
+         *
+         * @return
+         *  Success: length of value corresponding to key
+         *  Failure: -1
+         */
+        int db_store_get_value_size(
+            const std::string& table_b64,
+            const std::string& key_b64);
 
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-/**
- * Deletes a key->value pair from the database store
- *
- * @param table_b64     base64 encode table name
- * @param key_b64       base64 encoded key string
- * @param value_b64     base64 encoded value string
- *
- * @return
- *  Success: void/no return
- *  Failure: throws exception
- */
-void db_store_del(
-    const std::string& table_b64,
-    const std::string& key_b64,
-    const std::string& value_b64);
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        /**
+         * Gets the value corresponding to a key from the database store
+         *
+         * @param table_b64     base64 encoded table name
+         * @param key_b64       base64 encoded key string
+         *
+         * @return
+         *  Success: base64 encoded value corresponding to key
+         *  Failure: throws exception
+         */
+        std::string db_store_get(
+            const std::string& table_b64,
+            const std::string& key_b64);
 
+        // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        /**
+         * Puts a key->value pair into the database store
+         *
+         * @param table_b64     base64 encoded table name
+         * @param key_b64       base64 encoded key string
+         * @param value_b64     base64 encoded value string
+         *
+         * @return
+         *  Success: void/no return
+         *  Failure: throws exception
+         */
+        void db_store_put(
+            const std::string& table_b64,
+            const std::string& key_b64,
+            const std::string& value_b64);
+
+        // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        /**
+         * Deletes a key->value pair from the database store
+         *
+         * @param table_b64     base64 encoded table name
+         * @param key_b64       base64 encoded key string
+         * @param value_b64     base64 encoded value string
+         *
+         * @return
+         *  Success: void/no return
+         *  Failure: throws exception
+         */
+        void db_store_del(
+            const std::string& table_b64,
+            const std::string& key_b64,
+            const std::string& value_b64);
+};
--- shared_kv_storage/db_store/packages/db_store_wrapper.h
+++ shared_kv_storage/db_store/packages/db_store_wrapper.h
@@ -207,6 +207,51 @@ namespace db_store {
         const ByteArray& inId,
         const ByteArray& inValue);
 
+    // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    /**
+     * Updates a key->value pair in the database store. Appends/prepends a
+     * new string to the value separated by comma. The behavior is similar
+     * to pushing to the head/tail of a deque. Adds key-value pair if no
+     * entry is found.
+     * Primary expected use: python / untrusted side
+     *
+     * @param table     table name
+     * @param inId      id byte array
+     * @param inValue   data to be appended/written
+     *
+     * @return
+     *  TCF_SUCCESS  id->value updated/stored
+     *  else         failed, data store unchanged
+     */
+    tcf_err_t db_store_csv_extend(
+        const std::string& table,
+        const ByteArray& inId,
+        const ByteArray& inValue,
+        const bool isPrepend);
+
+    // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    /**
+     * Updates a key->value pair in the database store. Removes the first
+     * string from value which is a comma separated string. The behavior
+     * is similar to removing from the head of a deque. Deletes the
+     * key-value pair if this is the lone element in the value.
+     * Primary expected use: python / untrusted side
+     *
+     * @param table      table name
+     * @param inId       id byte array
+     * @param outValue   data to be written
+     *
+     * @return
+     *  TCF_SUCCESS    outValue contains the first element in the value
+     *                 field and it is removed from data store
+     *  TCF_ERR_VALUE  key was not present in the database store
+     *  else           failed, outValue unchanged and data store unchanged
+     */
+    tcf_err_t db_store_csv_pop(
+        const std::string& table,
+        const ByteArray& inId,
+        ByteArray& outValue);
+
     }  /* namespace db_store */
 
 
