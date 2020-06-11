1f7ee1d93514e6f960ba298ec3204208477d9f6f
Rajeev Ranjan <rajeev2.ranjan@intel.com>
2020-06-11 11:09:24 +0530

    Add new LMDB calls in proxy and handler
     - Add support for csv type values in kv
     - Update python handler and proxy
     - Update shared_kv_interface
    
    Signed-off-by: Rajeev Ranjan <rajeev2.ranjan@intel.com>

---
+++ shared_kv_storage/kv_storage/interface/kv_csv_interface.py
@@ -0,0 +1,72 @@
+# Copyright 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from abc import ABC, abstractmethod
+
+
+class KvCsvStorage(ABC):
+    """KvCsvStorage interface provides APIs to manipulate values
+       stored as csv in KV Storage."""
+
+    @abstractmethod
+    def csv_append(self, table, key, value):
+        """
+        Function to update a key-value pair in a lmdb table that holds
+        comma-separated strings as value. This function adds a string
+        to the end of the comma-separated value.
+
+        Parameters:
+           @param table - Name of the lmdb table in which key-value pair
+                          needs to be updated.
+           @param key - The primary key of the table.
+           @param value - The value that needs to be appended to the existing
+                          value(comma-separated) corresponding to the key.
+        """
+        pass
+
+# ---------------------------------------------------------------------------------------------------
+    @abstractmethod
+    def csv_prepend(self, table, key, value):
+        """
+        Function to update a key-value pair in a lmdb table that holds
+        comma-separated strings as value. This function adds a string
+        to the beginning of the comma-separated value.
+
+        Parameters:
+           @param table - Name of the lmdb table in which key-value pair
+                          needs to be updated.
+           @param key - The primary key of the table.
+           @param value - The value that needs to be prepended to the existing
+                          value(comma-separated) corresponding to the key.
+        """
+        pass
+
+# ---------------------------------------------------------------------------------------------------
+    @abstractmethod
+    def csv_pop(self, table, key):
+        """
+        Function to update a key-value pair in a lmdb table that holds
+        comma-separated strings as value. This function retrieves a string
+        from the beginning of the comma-separated value. It also deletes
+        the string from the value and if this is the lone string, the key-
+        value pair is removed.
+
+        Parameters:
+           @param table - Name of the lmdb table from which key-value pair
+                          needs to be read and updated.
+           @param key - The primary key of the table.
+        Returns:
+           @returns value - First element from comma-separated value for key
+                            passed in.
+        """
+        pass
---
+++ shared_kv_storage/kv_storage/remote_lmdb/db_store_csv.i
@@ -0,0 +1,66 @@
+/* Copyright 2020 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+%module db_store_csv
+
+%include <std_vector.i>
+%include <std_map.i>
+%include <std_string.i>
+
+namespace std {
+    %template(StringVector) vector<string>;
+    %template(StringMap) map<string, string>;
+}
+
+%include <exception.i>
+
+%exception {
+    try
+    {
+        $function
+    }
+    catch (db_error::MemoryError& e)
+    {
+        SWIG_exception(SWIG_MemoryError, e.what());
+    }
+    catch (db_error::IOError& e)
+    {
+        SWIG_exception(SWIG_IOError, e.what());
+    }
+    catch (db_error::RuntimeError& e)
+    {
+        SWIG_exception(SWIG_ValueError, e.what());
+    }
+    catch (db_error::DivisionByZero& e)
+    {
+        SWIG_exception(SWIG_DivisionByZero, e.what());
+    }
+    catch (db_error::UnknownError& e) {
+        SWIG_exception(SWIG_UnknownError, e.what());
+    }
+    catch (...)
+    {
+        SWIG_exception(SWIG_RuntimeError,"Unknown exception");
+    }
+}
+
+%{
+#include "db_store_csv.h"
+#include "db_store_error.h"
+%}
+
+
+%include "db_store_csv.h"
+
