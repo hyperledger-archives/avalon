5fa753a9745109904d7a0ae3f4d8f25f4e2224fb
Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@intel.com>
2020-07-01 14:58:16 +0530

    Enable fabric proxy model CI tests Rename fabric proxy model compose files
    Signed-off-by: Ramakrishna Srinivasamurthy <ramakrishna.srinivasamurthy@intel.com>

--- tools/run_proxy_model_tests.sh
+++ tools/run_proxy_model_tests.sh
@@ -32,7 +32,7 @@ try() {
 # time, so run test will wait for about 1min.
 
 yell "Wait for fabric blockchain connector to register worker"
-sleep 90s
+sleep 30s
 
 SCRIPTDIR="$(dirname $(readlink --canonicalize ${BASH_SOURCE}))"
 SRCDIR="$(realpath ${SCRIPTDIR}/..)"
