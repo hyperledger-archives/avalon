26d4e29ab2a7f851ae66a09f60d5467240085902
danintel <daniel.anderson@intel.com>
2020-07-02 09:19:19 -0700

    Add comments from tcf_error.h to enclave_error.py
    Also add missing comma separator and sort in numerical order
    
    Signed-off-by: danintel <daniel.anderson@intel.com>

--- common/python/error_code/enclave_error.py
+++ common/python/error_code/enclave_error.py
@@ -27,6 +27,8 @@ class EnclaveError(IntEnum):
     ENCLAVE_ERR_OVERFLOW = -7,
     ENCLAVE_ERR_VALUE = -8,
     ENCLAVE_ERR_SYSTEM = -9,
+    # The system is busy and the operation may be retried. If retries fail,
+    # this should be converted to ENCLAVE_ERR_SYSTEM for reporting.
+    ENCLAVE_ERR_SYSTEM_BUSY = -10,
     ENCLAVE_ERR_CRYPTO = -11,
-    ENCLAVE_ERR_SYSTEM_BUSY = -10
-    ENCLAVE_ERR_INVALID_WORKLOAD = -12
+    ENCLAVE_ERR_INVALID_WORKLOAD = -12 # Invalid workload ID
