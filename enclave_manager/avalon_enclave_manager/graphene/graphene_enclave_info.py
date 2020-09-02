3cb9ae7f8688bd8186e9f3adf6ab8a76a6243ba0
Rajeev Ranjan <rajeev2.ranjan@intel.com>
2020-09-02 08:37:41 +0530

    Generate AVR from quote for Graphene worker
    Signed-off-by: Rajeev Ranjan <rajeev2.ranjan@intel.com>

--- enclave_manager/avalon_enclave_manager/graphene/graphene_enclave_info.py
+++ enclave_manager/avalon_enclave_manager/graphene/graphene_enclave_info.py
@@ -14,6 +14,9 @@
 
 import logging
 from avalon_enclave_manager.enclave_attributes import EnclaveAttributes
+import random
+import json
+import avalon_enclave_manager.ias_client as ias_client
 
 logger = logging.getLogger(__name__)
 
@@ -26,22 +29,27 @@ class GrapheneEnclaveInfo(EnclaveAttributes):
     """
 
 # -------------------------------------------------------------------------
-    def __init__(self, worker_signup_json):
+    def __init__(self, config, worker_signup_json):
         """
         Constructor for GrapheneEnclaveInfo.
         Creates signup object.
         """
+        self._config = config['EnclaveModule']
         self.sealed_data = worker_signup_json["sealed_data"]
         self.verifying_key = worker_signup_json["verifying_key"]
         self.encryption_key = worker_signup_json["encryption_key"]
         self.encryption_key_signature = \
             worker_signup_json["encryption_key_signature"]
         self.enclave_id = self.verifying_key
-        # TODO: Generate IAS AVR
-        self.proof_data = None
+        quote = worker_signup_json["quote"]
+        if quote is None or len(quote) == 0:
+            self.proof_data = None
+        else:
+            self.proof_data = self._generate_avr(quote)
         self.extended_measurements = None
 
 # -------------------------------------------------------------------------
+
     def get_enclave_measurement(self):
         """
         A getter for enclave measurement
@@ -76,3 +84,55 @@ class GrapheneEnclaveInfo(EnclaveAttributes):
         return None
 
 # -----------------------------------------------------------------
+
+    def _connect_to_ias(self):
+        """
+        Connect to IAS
+        """
+        # IAS is not initialized in Intel SGX SIM mode. Need to check if SIM
+        # mode or HW mode. Only then connect to IAS.
+        self._ias = \
+            ias_client.IasClient(
+                IasServer=self._config['ias_url'],
+                ApiKey=self._config['ias_api_key'],
+                Spid=self._config['spid'],
+                HttpsProxy=self._config.get('https_proxy', ""))
+
+# -----------------------------------------------------------------
+
+    def _generate_avr(self, quote):
+        """
+        Generate Attestation Verification Report by contacting IAS.
+
+        Parameters:
+            @param quote - Quote generated within this enclave
+        Returns:
+            @returns A dictionary of AVR, report signature and signing
+                     certificate
+        """
+        ias_nonce = '{0:032X}'.format(random.getrandbits(128))
+        self._connect_to_ias()
+
+        logger.debug("About to post verification to IAS")
+        response = self._ias.post_verify_attestation(
+            quote=quote, nonce=ias_nonce)
+        # check verification report
+        if not self._ias.verify_report_fields(
+                quote, response['verification_report']):
+            logger.debug(
+                "last error: " + self._ias.last_verification_error())
+            if self._ias.last_verification_error() == "GROUP_OUT_OF_DATE":
+                logger.warning(
+                    "failure GROUP_OUT_OF_DATE " +
+                    "(update your BIOS/microcode!!!) keep going")
+            else:
+                logger.error("Invalid report fields")
+                return None
+        # ALL checks have passed
+        logger.info("Report fields verified")
+
+        return json.dumps({
+            'verification_report': response['verification_report'],
+            'ias_report_signature': response['ias_signature'],
+            'ias_report_signing_certificate': response['ias_certificate']
+        })
