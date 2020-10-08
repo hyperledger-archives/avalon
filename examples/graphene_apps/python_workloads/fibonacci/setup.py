ce15f13ddbbc1b789cb38c4a88964ac92f044bbd
manju956 <manjunath.a.c@intel.com>
2020-10-08 08:21:47 +0000

    Move graphene python worker to trusted module
    - the core of graphene python worker module, docker files from existing
      $TCF_HOME/example/graphene_apps/python_worker to trusted module i.e, $TCF_HOME/tc/graphene/python_worker
    - renamed $TCF_HOME/example/graphene_apps/python_worker to $TCF_HOME/example/graphene_apps/python_workloads
    - modified docker files to build restrustured python worker modules
    - moved ov_workload python module to $TCFHOME/example/graphene_apps/cppopenvino
    
    Signed-off-by: manju956 <manjunath.a.c@intel.com>

---
+++ examples/graphene_apps/python_workloads/fibonacci/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Copyright 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+# This should only be run with python3
+if sys.version_info[0] < 3:
+    print('ERROR: must run with python3')
+    sys.exit(1)
+
+from setuptools import setup, find_packages
+
+setup(name='avalon_python_fibonacci_workload',
+      version=0.6,
+      description='Avalon example fibonacci workload for Graphene',
+      author='Hyperledger Avalon',
+      url='https://github.com/hyperledger/avalon',
+      packages=find_packages(),
+      data_files=[],
+      entry_points={}
+      )
